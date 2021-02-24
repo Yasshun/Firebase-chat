b05faf7bba889a4395758f7a26b2e0da133d7e66
shunppe <70781900+shunppe@users.noreply.github.com>
2021-02-25 00:02:13 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Sidebar */}
+      {/* React Router -> Chat screen */}
+    </div>
+  )
+}
+
+export default App;
