5855b8285101193583bd854c84d97ee82bb6102e
shunppe <70781900+shunppe@users.noreply.github.com>
2021-03-01 23:44:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Header />
+      <div className="app__body"></div>
+        <Sidebar />
       {/* Sidebar */}
       {/* React Router -> Chat screen */}
     </div>
